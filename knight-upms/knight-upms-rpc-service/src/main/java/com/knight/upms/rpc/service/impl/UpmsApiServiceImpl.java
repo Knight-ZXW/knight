67be5ee9a170a31a5b7ede833a43fd4be1ae743e
Knight-ZXW <nimdanoob@163.com>
2017-12-14 00:44:32 +0800

    完成 UpmsApiServiceImpl

--- knight-upms/knight-upms-rpc-service/src/main/java/com/knight/upms/rpc/service/impl/UpmsApiServiceImpl.java
+++ knight-upms/knight-upms-rpc-service/src/main/java/com/knight/upms/rpc/service/impl/UpmsApiServiceImpl.java
@@ -1,6 +1,6 @@
 package com.knight.upms.rpc.service.impl;
 
-import com.knight.upms.dao.mapper.UpmsUserMapper;
+import com.knight.upms.dao.mapper.*;
 import com.knight.upms.dao.model.*;
 import com.knight.upms.rpc.api.UpmsApiService;
 import com.knight.upms.rpc.api.UpmsUserService;
@@ -8,6 +8,7 @@ import com.knight.upms.rpc.mapper.UpmsApiMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -20,6 +21,21 @@ public class UpmsApiServiceImpl implements UpmsApiService {
 
     @Autowired
     UpmsApiMapper upmsApiMapper;
+
+    @Autowired
+    UpmsRolePermissionMapper upmsRolePermissionMapper;
+
+    @Autowired
+    UpmsUserPermissionMapper upmsUserPermissionMapper;
+
+    @Autowired
+    UpmsSystemMapper upmsSystemMapper;
+
+    @Autowired
+    UpmsOrganizationMapper upmsOrganizationMapper;
+
+    @Autowired
+    UpmsLogMapper upmsLogMapper;
     /**
      * 根据用户id 获取所拥有的权限
      * @param upmsUserId
@@ -36,48 +52,117 @@ public class UpmsApiServiceImpl implements UpmsApiService {
         return upmsPermissions;
     }
 
+    /**
+     * 根据用户id获取所拥有的权限
+     * @param upmsUserId
+     * @return
+     */
     @Override
+    @Cacheable(value = "zheng-upms-rpc-service-ehcache", key = "'selectUpmsPermissionByUpmsUserId_' + #upmsUserId")
     public List<UpmsPermission> selectUpmsPermissionByUpmsUserIdByCache(Integer upmsUserId) {
-        return null;
+        return selectUpmsPermissionByUpmsUserId(upmsUserId);
     }
 
+    /**
+     * 根据用户id获取所属的角色
+     * @param upmsUserId
+     * @return
+     */
     @Override
     public List<UpmsRole> selectUpmsRoleByUpmsUserId(Integer upmsUserId) {
-        return null;
+        // 用户不存在或锁定状态
+        UpmsUser upmsUser = upmsUserMapper.selectByPrimaryKey(upmsUserId);
+        if (null == upmsUser || 1 == upmsUser.getLocked()) {
+            _log.info("selectUpmsRoleByUpmsUserId : upmsUserId={}", upmsUserId);
+            return null;
+        }
+        List<UpmsRole> upmsRoles = upmsApiMapper.selectUpmsRoleByUpmsUserId(upmsUserId);
+        return upmsRoles;
     }
 
+    /**
+     * 根据用户id获取所属的角色
+     * @param upmsUserId
+     * @return
+     */
     @Override
+    @Cacheable(value = "knight-upms-rpc-service-ehcache", key = "'selectUpmsRoleByUpmsUserId_' + #upmsUserId")
     public List<UpmsRole> selectUpmsRoleByUpmsUserIdByCache(Integer upmsUserId) {
-        return null;
+        return selectUpmsRoleByUpmsUserId(upmsUserId);
     }
 
+    /**
+     * 根据角色id获取所拥有的权限
+     * @param upmsRoleId
+     * @return
+     */
     @Override
     public List<UpmsRolePermission> selectUpmsRolePermisstionByUpmsRoleId(Integer upmsRoleId) {
-        return null;
+        UpmsRolePermissionExample upmsRolePermissionExample = new UpmsRolePermissionExample();
+        upmsRolePermissionExample.createCriteria()
+                .andRoleIdEqualTo(upmsRoleId);
+        List<UpmsRolePermission> upmsRolePermissions = upmsRolePermissionMapper.selectByExample(upmsRolePermissionExample);
+        return upmsRolePermissions;
     }
 
+    /**
+     * 根据用户id获取所拥有的权限
+     * @param upmsUserId
+     * @return
+     */
     @Override
     public List<UpmsUserPermission> selectUpmsUserPermissionByUpmsUserId(Integer upmsUserId) {
-        return null;
+        UpmsUserPermissionExample upmsUserPermissionExample = new UpmsUserPermissionExample();
+        upmsUserPermissionExample.createCriteria()
+                .andUserIdEqualTo(upmsUserId);
+        List<UpmsUserPermission> upmsUserPermissions = upmsUserPermissionMapper.selectByExample(upmsUserPermissionExample);
+        return upmsUserPermissions;
     }
 
+    /**
+     * 根据条件获取系统数据
+     * @param upmsSystemExample
+     * @return
+     */
     @Override
     public List<UpmsSystem> selectUpmsSystemByExample(UpmsSystemExample upmsSystemExample) {
-        return null;
+        return upmsSystemMapper.selectByExample(upmsSystemExample);
     }
 
+    /**
+     * 根据条件获取组织数据
+     * @param upmsOrganizationExample
+     * @return
+     */
     @Override
     public List<UpmsOrganization> selectUpmsOrganizationByExample(UpmsOrganizationExample upmsOrganizationExample) {
-        return null;
+        return upmsOrganizationMapper.selectByExample(upmsOrganizationExample);
     }
 
+    /**
+     * 根据username获取UpmsUser
+     * @param username
+     * @return
+     */
     @Override
     public UpmsUser selectUpmsUserByUsername(String username) {
+        UpmsUserExample upmsUserExample = new UpmsUserExample();
+        upmsUserExample.createCriteria()
+                .andUsernameEqualTo(username);
+        List<UpmsUser> upmsUsers = upmsUserMapper.selectByExample(upmsUserExample);
+        if (null != upmsUsers && upmsUsers.size() > 0) {
+            return upmsUsers.get(0);
+        }
         return null;
     }
 
+    /**
+     * 写入操作日志
+     * @param record
+     * @return
+     */
     @Override
     public int insertUpmsLogSelective(UpmsLog record) {
-        return 0;
+        return upmsLogMapper.insertSelective(record);
     }
 }
